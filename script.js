90961f0ae15b7d76ba42bb1e4d11c5363d269b98
javibeat <javibeat@gmail.com>
2024-03-27 13:13:56 +0400

    asdf

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ function initializeMenu() {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
 
+    // Esto manejará el clic en el ícono de hamburguesa.
     hamburger.addEventListener("click", () => {
+        // Agrega o quita la clase 'active' al ícono de hamburguesa y al menú.
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
     });
 
+    // Esto manejará el clic en cada enlace del menú.
     document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+        // Remueve la clase 'active' del ícono de hamburguesa y del menú para cerrarlo.
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     }));
